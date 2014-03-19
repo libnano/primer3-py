7b375363fd4f23958687fc412b1f1b5067e3ecea
benpruitt <bpruittvt@gmail.com>
2014-03-18 23:07:38 -0400

    better temporary file handling in wrappers module

--- primer3/wrappers.py
+++ primer3/wrappers.py
@@ -9,136 +9,14 @@ import os
 import re
 import subprocess
 
+from tempfile import mkstemp
 from collections import OrderedDict, namedtuple
 
 from os.path import join as pjoin
 
 
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ UTILITY FUNCTIONS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
-
-# Underscore imports for TermporaryDirectory (see issue #10188)
-import warnings as _warnings
-import sys as _sys
-import os as _os
-
-import atexit
-
-from tempfile import mkdtemp
-from uuid import uuid4
-
-
-# Although it does not have an underscore for historical reasons, this
-# variable is an internal implementation detail (see issue 10354).
-template = 'tmp'
-
-class TemporaryDirectory(object):
-    '''Create and return a temporary directory. This has the same
-    behavior as mkdtemp but can be used as a context manager. For
-    example:
-
-        with TemporaryDirectory() as tmpdir:
-            ...
-
-    Upon exiting the context, the directory and everything contained
-    in it are removed. This is an augmented version of the class included
-    in the Python 3 tempfile module and it includes methods related to
-    module-persistent temperorary directories that are cleaned up at exit.
-    '''
-
-    def __init__(self, suffix='', prefix=template, dir=None, persist=False):
-        self._closed = False
-        self.name = None # Handle mkdtemp raising an exception
-        self.name = mkdtemp(suffix, prefix, dir)
-        if persist:
-            self.persist_until_exit()
-
-    def __repr__(self):
-        return '<{} {!r}>'.format(self.__class__.__name__, self.name)
-
-    def __enter__(self):
-        return self.name
-
-    def cleanup(self, _warn=False):
-        if self.name and not self._closed:
-            try:
-                self._rmtree(self.name)
-            except (TypeError, AttributeError) as ex:
-                # Issue #10188: Emit a warning on stderr
-                # if the directory could not be cleaned
-                # up due to missing globals
-                if 'None' not in str(ex):
-                    raise
-                print('ERROR: {!r} while cleaning up {!r}'.format(ex, self,),
-                      file=_sys.stderr)
-                return
-            self._closed = True
-            if _warn:
-                self._warn('Implicitly cleaning up {!r}'.format(self),
-                           Warning)
-
-    def persist_until_exit(self):
-        atexit.register(self.cleanup)
-
-    def __exit__(self, exc, value, tb):
-        self.cleanup()
-
-    def __del__(self):
-        # Issue a ResourceWarning if implicit cleanup needed
-        self.cleanup(_warn=True)
-
-    # XXX (ncoghlan): The following code attempts to make
-    # this class tolerant of the module nulling out process
-    # that happens during CPython interpreter shutdown
-    # Alas, it doesn't actually manage it. See issue #10188
-    _listdir = staticmethod(_os.listdir)
-    _path_join = staticmethod(_os.path.join)
-    _isdir = staticmethod(_os.path.isdir)
-    _islink = staticmethod(_os.path.islink)
-    _remove = staticmethod(_os.remove)
-    _rmdir = staticmethod(_os.rmdir)
-    _warn = _warnings.warn
-
-    def _rmtree(self, path):
-        # Essentially a stripped down version of shutil.rmtree.  We can't
-        # use globals because they may be None'ed out at shutdown.
-        for name in self._listdir(path):
-            fullname = self._path_join(path, name)
-            try:
-                isdir = self._isdir(fullname) and not self._islink(fullname)
-            except OSError:
-                isdir = False
-            if isdir:
-                self._rmtree(fullname)
-            else:
-                try:
-                    self._remove(fullname)
-                except OSError:
-                    pass
-        try:
-            self._rmdir(path)
-        except OSError:
-            pass
-
-
-def unique_fn(directory, file_ext=''):
-    ''' Generates a unique filename and returns the full file path
-
-    Insures that the file does not already exist but does not return an
-    open file (returns the filepath as a string)'''
-
-    while True:
-        fp = _os.path.join(directory, str(uuid4()) + file_ext)
-        try:
-            with open(fp, 'rb') as fd:
-                pass
-        except:
-            return fp
-
-
-
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ PRIMER3 WRAPPERS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
-TMP_DIR = TemporaryDirectory(persist=True).name
 DEV_NULL = open(os.devnull, 'wb')
 
 local_dir = os.path.dirname(os.path.realpath(__file__))
@@ -188,7 +66,7 @@ def calcTm(seq, mv_conc=50, dv_conc=0, dntp_conc=0.8, dna_conc=50,
             '-tp',  str(tm_meth),
             '-sc',  str(salt_meth),
             seq]
-    tm = subprocess.check_output(args, cwd=TMP_DIR, stderr=DEV_NULL,
+    tm = subprocess.check_output(args, stderr=DEV_NULL,
                                  env=os.environ)
     return float(tm)
 
@@ -230,7 +108,7 @@ def calcThermo(seq1, seq2, calc_type='ANY', mv_conc=50, dv_conc=0,
             '-s2',      seq2]
     if temp_only:
         args += ['-r']
-    out = subprocess.check_output(args, cwd=TMP_DIR, stderr=DEV_NULL,
+    out = subprocess.check_output(args, stderr=DEV_NULL,
                                   env=os.environ)
     return _parse_ntthal(out)
 
@@ -291,6 +169,7 @@ def _writeBoulderIO(fp, p3_args):
         for k, v in p3_args.items():
             fd.write(bytes('{}={}\n'.format(k,v), 'UTF-8'))
         fd.write(bytes('=', 'UTF-8'))
+        fd.flush()
 
 
 def _parseBoulderIO(fp):
@@ -307,10 +186,17 @@ def runP3Main(p3_args):
 
     Returns an ordered dict of the boulderIO-format primer3 output file
     '''
-    fp_in = unique_fn(TMP_DIR, file_ext='.in')
+    fd_in, fp_in = mkstemp(suffix='.in')
+    fd_out, fp_out = mkstemp(suffix='.out')
+    os.close(fd_in)
+    os.close(fd_out)
     _writeBoulderIO(fp_in, p3_args)
-    fp_out = fp_in.rstrip('.in') + '.out'
     subprocess.check_call([pjoin(PRIMER3_SRC, 'primer3_core'), '--output',
-                           fp_out, fp_in], env=os.environ)
-    return _parseBoulderIO(fp_out)
-
+                          fp_out, fp_in], env=os.environ)
+    out_dict = _parseBoulderIO(fp_out)
+    try:
+        os.remove(fp_in)
+        os.remove(fp_out)
+    except IOError:
+        pass
+    return out_dict
