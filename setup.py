e587f4c4fc5d6aef71e4fc93d39f52a9548716f3
Ben <bpruittvt@gmail.com>
2014-08-11 18:15:31 -0400

    add flag for python 3.4 declaration after statement compile error

--- setup.py
+++ setup.py
@@ -68,6 +68,7 @@ p3_files = [rpath(pjoin(root, f), package_path) for root, _, files in
 primer3_ext = Extension('_primer3',
                         sources=['primer3/src/primer3_py.c'] + primer3_srcs,
                         include_dirs=[primer3_src, klib_src],
+                        extra_compile_args=["-Wno-error=declaration-after-statement"]
                         )
 
 print('Module Setup'.center(80, '*'))
