68545a24db257b638aa7f70f6812cb3adcbef25f
benpruitt <bpruittvt@gmail.com>
2014-11-25 20:21:32 -0500

    update docs

--- docs/conf.py
+++ docs/conf.py
@@ -51,7 +51,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'primer3-py'
+project = u'Primer3-py'
 copyright = u'2014, Ben Pruitt, Nick Conway; Wyss Institute'
 
 # The version info for the project you're documenting, acts as replacement for
