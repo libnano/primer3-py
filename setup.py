405b0afb7ba0386b697169ca1ef38fb3dda142d2
benpruitt <bpruittvt@gmail.com>
2020-05-09 17:38:31 -0400

    cleanup and update setup.py for better windows support

--- setup.py
+++ setup.py
@@ -54,62 +54,84 @@ from os.path import relpath as rpath
 with open('README.rst') as fd:
     LONG_DESCRIPTION = fd.read()
 
+# Platform-dependent binary for `make` commands
+MAKE_BIN = 'mingw32-make' if sys.platform == 'win32' else 'make'
+# Indicates whether the Primer3 library has been built during install
+P3_BUILT = False
 
-PACKAGE_PATH =          os.path.abspath(os.path.dirname(__file__))
-MODULE_PATH =           pjoin(PACKAGE_PATH, 'primer3')
-SRC_PATH =              pjoin(MODULE_PATH, 'src')
-TESTS_PATH =            pjoin(MODULE_PATH, 'tests')
-LIBPRIMER3_PATH =       pjoin(SRC_PATH, 'libprimer3')
-THERMO_PARAMS_PATH =    pjoin(LIBPRIMER3_PATH, 'primer3_config')
-KLIB_PATH =             pjoin(LIBPRIMER3_PATH, 'klib')
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Package paths ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
-libprimer3_paths = [rpath(pjoin(LIBPRIMER3_PATH, 'thal.c')),
-                    rpath(pjoin(LIBPRIMER3_PATH, 'oligotm.c')),
-                    rpath(pjoin(LIBPRIMER3_PATH, 'p3_seq_lib.c')),
-                    rpath(pjoin(LIBPRIMER3_PATH, 'libprimer3.c')),
-                    rpath(pjoin(LIBPRIMER3_PATH, 'dpal.c')),
-                    rpath(pjoin(SRC_PATH, 'primerdesign_helpers.c'))]
+PACKAGE_PATH = os.path.abspath(os.path.dirname(__file__))
+MODULE_PATH = pjoin(PACKAGE_PATH, 'primer3')
+SRC_PATH = pjoin(MODULE_PATH, 'src')
+TESTS_PATH = pjoin(MODULE_PATH, 'tests')
+LIBPRIMER3_PATH = pjoin(SRC_PATH, 'libprimer3')
+THERMO_PARAMS_PATH = pjoin(LIBPRIMER3_PATH, 'primer3_config')
+KLIB_PATH = pjoin(LIBPRIMER3_PATH, 'klib')
 
+LIBPRIMER3_FPS = [
+    rpath(pjoin(LIBPRIMER3_PATH, 'thal.c')),
+    rpath(pjoin(LIBPRIMER3_PATH, 'oligotm.c')),
+    rpath(pjoin(LIBPRIMER3_PATH, 'p3_seq_lib.c')),
+    rpath(pjoin(LIBPRIMER3_PATH, 'libprimer3.c')),
+    rpath(pjoin(LIBPRIMER3_PATH, 'dpal.c')),
+    rpath(pjoin(SRC_PATH, 'primerdesign_helpers.c')),
+]
 
-maker = 'nmake' if sys.platform == 'win32' else 'make'
+LIBPRIMER3_BINARIES = ['oligotm', 'ntthal', 'primer3_core']
+if sys.platform == 'win32':
+    LIBPRIMER3_BINARIES = [bin_fn + '.exe' for bin_fn in LIBPRIMER3_BINARIES]
+
+LIBPRIMER3_BINARY_FPS = [
+    pjoin(LIBPRIMER3_PATH, fn) for fn in LIBPRIMER3_BINARIES
+]
+
+LIBPRIMER3_THERMO_FPS = [
+    rpath(pjoin(root, fp), MODULE_PATH) for root, _, fps in
+    os.walk(THERMO_PARAMS_PATH) for fp in fps
+]
+
+LIBPRIMER3_TEST_FPS = [
+    rpath(pjoin(root, fp), MODULE_PATH) for root, _, fps in
+    os.walk(TESTS_PATH) for fp in fps
+]
+
+PACKAGE_FPS = (
+    LIBPRIMER3_THERMO_FPS +
+    LIBPRIMER3_TEST_FPS +
+    ['thermoanalysis.pxd', 'thermoanalysis.pyx']
+)
+
+# ~~~~~~~~~~~~~~~~~~~~~ Primer3 C library build helpers ~~~~~~~~~~~~~~~~~~~~~ #
 
 def p3Clean():
-    # Clean up any previous primer3 builds
-    if sys.platform != 'win32':
-        p3clean = subprocess.Popen(['%s clean' % (maker)], shell=True,
-                                   cwd=LIBPRIMER3_PATH)
-        p3clean.wait()
+    '''
+    Run `make clean` for libprimer3 in a platform-dependent manner
+    '''
+    proc = subprocess.Popen(
+        '{} clean'.format(MAKE_BIN),
+        shell=True,
+        cwd=LIBPRIMER3_PATH,
+    )
+    proc.wait()
 
 
 def p3Build():
-    # Build primer3
-    if sys.platform != 'win32':
-        p3build = subprocess.Popen(['%s clean; %s' % (maker, maker)], shell=True,
-                                   cwd=LIBPRIMER3_PATH)
-        p3build.wait()
-
-P3_BUILT = False
-
-
-# Find all necessary primer3 binaries / data files to include with the package
-p3_binaries = ['oligotm', 'ntthal', 'primer3_core']
-if sys.platform == 'win32':
-    p3_binaries = []
-
-p3_binary_fps = [pjoin(LIBPRIMER3_PATH, fn) for fn in p3_binaries]
-
-thermo_files = [rpath(pjoin(root, f), MODULE_PATH) for root, _, files in
-                os.walk(THERMO_PARAMS_PATH) for f in files]
-
-test_files = [rpath(pjoin(root, f), MODULE_PATH) for root, _, files in
-                os.walk(TESTS_PATH) for f in files]
-
-p3_files = thermo_files + test_files + ['thermoanalysis.pxd', 'thermoanalysis.pyx']
+    '''
+    Run `make clean && make` for libprimer3 in a platform-dependent manner
+    '''
+    proc = subprocess.Popen(
+        '{} clean; {}'.format(MAKE_BIN, MAKE_BIN),
+        shell=True,
+        cwd=LIBPRIMER3_PATH,
+    )
+    proc.wait()
 
 
 # Insure that the copied binaries are executable
 def makeExecutable(fp):
-    ''' Adds the executable bit to the file at filepath `fp`
+    '''
+    Adds the executable bit to the file at filepath `fp`
     '''
     mode = ((os.stat(fp).st_mode) | 0o555) & 0o7777
     setup_log.info("Adding executable bit to %s (mode is now %o)", fp, mode)
@@ -117,80 +139,101 @@ def makeExecutable(fp):
 
 
 class CustomInstallLib(install_lib.install_lib):
+    '''
+    Custom library installer to ensure that libprimer3 binaries are installed
+    and made executable.
 
+    Installed and invoked internally by `setuptools`/`distutils`
+    '''
     def run(self):
         global P3_BUILT
-        install_lib.install_lib.run(self)
-        # Copy binary files over to build directory and make executable
-        if not self.dry_run:
-            if not P3_BUILT:
-                p3Clean()
-                p3Build()
-                P3_BUILT = True
-            new_p3_binary_fps = [pjoin(self.install_dir, 'primer3', 'src',
-                                 'libprimer3', fn) for fn in p3_binaries]
-            [shutil.copyfile(o, d) for o, d in zip(p3_binary_fps,
-                                                   new_p3_binary_fps)]
-            list(map(makeExecutable, new_p3_binary_fps))
+        super().run()
+        if not P3_BUILT:
+            p3Clean()
+            p3Build()
+            P3_BUILT = True
+        binary_dest_fps = [
+            pjoin(
+                self.install_dir,
+                'primer3',
+                'src',
+                'libprimer3',
+                fn,
+            ) for fn in LIBPRIMER3_BINARIES
+        ]
+        for src_fp, dest_fp in zip(LIBPRIMER3_BINARY_FPS, binary_dest_fps):
+            shutil.copyfile(src_fp, dest_fp)
+            makeExecutable(dest_fp)
 
 
 class CustomSdist(sdist.sdist):
+    '''
+    Custom sdist packager, ensures that libprimer3 build artifacts are removed
+    prior to packaging.
 
+    Installed and invoked internally by `setuptools`/`distutils`
+    '''
     def run(self):
         global P3_BUILT
         # Clean up the primer3 build prior to sdist command to remove
         # binaries and object/library files
         p3Clean()
         P3_BUILT = False
-        sdist.sdist.run(self)
+        # Remove the build Cython
+        os.remove(os.path.join(MODULE_PATH, 'thermoanalysis.c'))
+        super().run()
 
 
 class CustomBuildClib(build_clib.build_clib):
+    '''
+    Custom C library builder, ensures that libprimer3 is built prior to C
+    library builds.
 
+    Installed and invoked internally by `setuptools`/`distutils`
+    '''
     def run(self):
         global P3_BUILT
         # Build primer3 prior to building the extension, if not already built
-        if not self.dry_run and not P3_BUILT:
+        if not P3_BUILT:
             p3Clean()
             p3Build()
             P3_BUILT = True
-        build_clib.build_clib.run(self)
+        super().run()
 
 
-# Build the C API and Cython extensions
+# ~~~~~~~~~~~~~~~~~~~~~~ Cython / C API extension setup ~~~~~~~~~~~~~~~~~~~~~ #
+
 if sys.platform == 'win32':
-    extra_compile_args = ['']
+    EXTRA_COMPILE_ARGS = ['']
 else:
-    extra_compile_args = ['-Wno-error=declaration-after-statement',
-                        '-Wno-unused-function']
+    EXTRA_COMPILE_ARGS = [
+        '-Wno-error=declaration-after-statement',
+        '-Wno-unused-function',
+    ]
 
 primerdesign_ext = Extension(
     'primer3.primerdesign',
-    sources=[pjoin('primer3','src','primerdesign_py.c')] + libprimer3_paths,
+    sources=[pjoin('primer3','src','primerdesign_py.c')] + LIBPRIMER3_FPS,
     include_dirs=[LIBPRIMER3_PATH, KLIB_PATH],
-    extra_compile_args=extra_compile_args
+    extra_compile_args=EXTRA_COMPILE_ARGS,
 )
 
 
 thermoanalysis_ext = Extension(
     'primer3.thermoanalysis',
-    sources=[pjoin('primer3','thermoanalysis.pyx')] + libprimer3_paths,
+    sources=[pjoin('primer3','thermoanalysis.pyx')] + LIBPRIMER3_FPS,
     include_dirs=[LIBPRIMER3_PATH, KLIB_PATH],
-    extra_compile_args=extra_compile_args
+    extra_compile_args=EXTRA_COMPILE_ARGS,
 )
 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
-
+# Ensure that libprimer3 is built prior to the `build_ext` or `install` cmds
 if ('build_ext' in sys.argv or 'install' in sys.argv):
     if not P3_BUILT:
         p3Clean()
         p3Build()
         P3_BUILT = True
-
-# Insure that we don't include the built Cython module in the dist
-if 'sdist' in sys.argv:
-    os.remove(os.path.join(MODULE_PATH, 'thermoanalysis.c'))
-
 
 setup(
     name='primer3-py',
@@ -208,6 +251,8 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Development Status :: 4 - Beta',
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering :: Bio-Informatics',
@@ -215,10 +260,13 @@ setup(
     ],
     packages=['primer3'],
     ext_modules=[primerdesign_ext, thermoanalysis_ext],
-    package_data={'primer3': p3_files},
-    cmdclass={'install_lib': CustomInstallLib, 'sdist': CustomSdist,
-              'build_clib': CustomBuildClib},
+    package_data={'primer3': PACKAGE_FPS},
+    cmdclass={
+        'install_lib': CustomInstallLib,
+        'sdist': CustomSdist,
+        'build_clib': CustomBuildClib,
+    },
     test_suite='tests',
     setup_requires=['Cython', 'setuptools>=18.0'],
-    zip_safe=False
+    zip_safe=False,
 )
