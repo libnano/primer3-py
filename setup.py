781df2a008fb80d0e67e3fbdb0274cfe2e612c32
Ben Pruitt <bpruittvt@gmail.com>
2024-02-09 09:23:00 -0500

    support for python 3.12 (build, test, docs) (#129)

--- setup.py
+++ setup.py
@@ -367,6 +367,7 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'Development Status :: 4 - Beta',
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering :: Bio-Informatics',
