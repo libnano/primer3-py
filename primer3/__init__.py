4262d64b3d9f4d966f8a2bddf06ea8a428323605
benpruitt <bpruittvt@gmail.com>
2025-02-17 13:33:51 -0500

    bump version to 2.1.0

--- primer3/__init__.py
+++ primer3/__init__.py
@@ -26,7 +26,7 @@ import os
 from typing import List
 
 # Per PEP-440 https://peps.python.org/pep-0440/#public-version-identifiers
-__version__ = '2.0.3'
+__version__ = '2.1.0'
 __author__ = 'Ben Pruitt, Nick Conway'
 __copyright__ = (
     'Copyright 2014-2025, Ben Pruitt & Nick Conway; 2014-2018 Wyss Institute'
